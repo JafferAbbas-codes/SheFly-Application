17b45436d98134812d01348ebf5b162a03020e32
ibrahim <ibrahimiqbal2011@gmail.com>
2021-04-02 01:19:03 +0500

    Add Feedback and AcceptBid screen

---
+++ screens/Buyer/bidaccepted.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import {View, Text} from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/FontAwesome';
+
+export default function offerSent() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignSelf: 'center',
+        justifyContent: 'center',
+      }}>
+      <MaterialIcons
+        name="check-circle"
+        size={180}
+        style={{
+          color: '#AD379D',
+        }}
+      />
+      <Text style={{textAlign: 'center', fontSize: 30}}>Bid Accepted</Text>
+    </View>
+  );
+}
